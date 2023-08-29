0b19fafc81b24055b529b2854579890fb0698858
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-08-29 14:22:50 -0400

    fix: remove example

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -16,7 +16,7 @@ export const exampleRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return "Todo!";
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
