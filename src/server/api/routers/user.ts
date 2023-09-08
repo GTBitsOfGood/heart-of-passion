6f27c8a111df2bfa8aed9f71fd8f29ca2598384f
Keerth1Keerthi <KeerthiKonuganti@gmail.com>
2023-09-08 12:04:52 -0400

    feat: createUser functionality

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -7,15 +7,18 @@ import {
 } from "~/server/api/trpc";
 
 import { getGlobalCount, incrementGlobalCount } from "~/server/actions/Count";
-
+import dbConnect from "~/server/db";
+import { Model } from '~/server/models/User'
 export const userRouter = createTRPCRouter({
     createUser: publicProcedure
         .input(z.object({
             name: z.string().optional(), email: z.string(),
             role: z.string(), chapter: z.string().optional()
         }))
-        .mutation(({ input }) => {
-            console.log(`User created`)
+        .mutation(async ({ input }) => {
+            await dbConnect();
+            const user = new Model(input)
+            await user.save();
             return {
                 success: `User created`,
             };
