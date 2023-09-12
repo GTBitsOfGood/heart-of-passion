4c9846422ba81c38cd6279903241d376fa02f67d
rdong46 <rdong46@gatech.edu>
2023-09-12 17:58:25 -0400

    setup user page

---
+++ src/pages/users.tsx
@@ -0,0 +1,13 @@
+import { Button, Heading, Text } from "@chakra-ui/react";
+
+import { api } from "~/utils/api";
+
+export default function Users() {
+  const users = "Users";
+
+  return (
+    <>
+      <Heading>users</Heading>
+    </>
+  );
+}
