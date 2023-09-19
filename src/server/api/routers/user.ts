bcdf29bc91ec6c5d8c972897ea3f06719bbab06f
Keerth1Keerthi <112666022+Keerth1Keerthi@users.noreply.github.com>
2023-09-19 01:12:49 -0400

    Routers (#17)
    * feat: created user router
    
    * feat: createUser functionality
    
    * feat: deleteUser functionality
    
    * feat: updateUser functionality
    
    * feat: getUser and getUsers functionality
    
    * feat: chapter router
    
    * fix: changed return statements for router methods
    
    * fix: error handling with try catch
    
    * feat: error handling for chapter router
    
    * fix: remove dbConnect in routers
    
    * fix: typo

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,93 @@
+import { z } from "zod";
+
+import {
+    createTRPCRouter,
+    protectedProcedure,
+    publicProcedure,
+} from "~/server/api/trpc";
+
+import { getGlobalCount, incrementGlobalCount } from "~/server/actions/Count";
+import { Model } from '~/server/models/User'
+export const userRouter = createTRPCRouter({
+    createUser: publicProcedure
+        .input(z.object({
+            name: z.string().optional(), email: z.string(),
+            role: z.string(), chapter: z.string().optional()
+        }))
+        .mutation(async ({ input }) => {
+            const user = new Model(input)
+            await user.save();
+            return user;
+        }),
+    deleteUser: publicProcedure
+        .input(z.string())
+        .mutation(async (opts) => {
+            try {
+                console.log(opts.input)
+                const user = await Model.findOneAndDelete({ email: opts.input })
+                return {
+                    success: true,
+                    message: user
+                }
+            } catch (e) {
+                return {
+                    success: false,
+                    message: e
+                }
+            }
+        }),
+    updateUser: publicProcedure
+        .input(z.object({
+            email: z.string(),
+            updateData: z.object({
+                name: z.string().optional(), email: z.string().optional(),
+                role: z.string().optional(), chapter: z.string().optional()
+            })
+        }
+        ))
+        .mutation(async (opts) => {
+            try {
+                const user = await Model.findOneAndUpdate({ email: opts.input.email }, opts.input.updateData)
+                return {
+                    success: true,
+                    message: user
+                };
+            } catch (e) {
+                return {
+                    success: false,
+                    message: e
+                }
+            }
+        }),
+    getUser: publicProcedure
+        .input(z.string())
+        .query(async (opts) => {
+            try {
+                const user = await Model.findOne({ email: opts.input });
+                return {
+                    success: true,
+                    message: user
+                };
+            } catch (e) {
+                return {
+                    success: false,
+                    message: e
+                }
+            }
+        }),
+    getUsers: publicProcedure
+        .query(async (opts) => {
+            try {
+                const users = await Model.find();
+                return {
+                    success: true,
+                    message: users
+                };
+            } catch (e) {
+                return {
+                    success: false,
+                    message: e
+                }
+            }
+        })
+});
