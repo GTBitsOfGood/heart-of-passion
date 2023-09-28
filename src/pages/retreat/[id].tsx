10757656fd6e262f713fc67365a3c642ddecddd1
Syaam Khandaker <syaamkhandaker@gmail.com>
2023-09-28 18:26:57 -0400

    initial code

---
+++ src/pages/retreat/[id].tsx
@@ -0,0 +1,150 @@
+import { useRouter } from "next/router";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import Card from "./card";
+import "@fontsource/oswald/700.css";
+
+const sampleData = [
+  [
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+  ],
+
+  [
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+  ],
+
+  [
+    {
+      startTime: "9:30 am",
+      endTime: "4:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+  ],
+
+  [
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+    {
+      startTime: "9:30 am",
+      endTime: "12:00 pm",
+      task: "Breakfast",
+      address: "123 First Drive",
+      price: 345,
+      priority: "LOW",
+    },
+  ],
+];
+export default function Calendar() {
+  const router = useRouter();
+  return (
+    <Box>
+      <Flex justifyContent="center" alignItems="center">
+        <Box>{/*Sidebar*/}</Box>
+        <Box display={"flex"} gap={"34px"}>
+          {sampleData.map((day, index) => {
+            return (
+              <Box>
+                <Text
+                  width={"158px"}
+                  height={"81px"}
+                  fontSize={36}
+                  fontWeight={700}
+                  textAlign={"center"}
+                  fontFamily={"oswald"}
+                >
+                  Day {index + 1}
+                </Text>
+                <Box display={"flex"}>
+                  <Box marginRight={"34px"}>
+                    {day.map((event) => {
+                      return <Card event={event} key={index} />;
+                    })}
+                  </Box>
+                  <Box width={"1px"} background={"#989898"} height={"788px"} />
+                </Box>
+              </Box>
+            );
+          })}
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
