bfa3d0da2c6ed0da40c7c2257eb51bb2cf2a1a97
Syaam Khandaker <syaamkhandaker@gmail.com>
2023-09-28 19:24:42 -0400

    updated types

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -8,3 +8,49 @@ export type User = {
 export type Chapter = {
   name: string;
 }
+
+export enum EnergyLevel {
+  Low = 'low',
+  Medium = 'medium',
+  High = 'high'
+}
+
+export enum Category {
+  Entertainment = 'entertainment',
+  Educational = 'educational',
+  Other = 'other'
+}
+
+export enum ExpenseType {
+  Entertainment = 'entertainment',
+  Transportation = 'transportation',
+  Other = 'other'
+}
+
+export enum CostType {
+  PerUnit = 'per unit',
+  FlatCost = 'flat cost'
+}
+
+export interface DateObject {
+  day: number;
+  from: string;
+  to: string;
+}
+
+export interface ExpenseObject {
+  name: string;
+  type: ExpenseType;
+  costType: CostType;
+  numberOfUnits?: number;
+  notes?: string;
+}
+
+export type Event = {
+  name: string;
+  location?: string;
+  energyLevel?: EnergyLevel;
+  category?: Category;
+  dates: DateObject[];
+  expenses: ExpenseObject[];
+}
