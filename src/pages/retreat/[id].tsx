c0c2b9a6b0d27edbca76be03da7f9630a5462b76
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2023-10-30 19:01:28 -0400

    Start attempting sidebar fix

--- src/pages/retreat/[id].tsx
+++ src/pages/retreat/[id].tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import CalendarCard from "src/components/Calendar/CalendarCard";
 
 import "@fontsource/oswald/700.css";
@@ -7,94 +7,99 @@ import { useEffect, useState } from "react";
 import { trpc } from "~/utils/api";
 import { DateObject, Chapter } from "~/common/types";
 import Sidebar from "~/components/Sidebar";
+import { IEvent } from "~/server/models/Event";
+
+function Content({ events }: { events: IEvent[] }) {
+  return (
+    <Flex justifyContent="center" alignItems="center">
+      <Box display={"flex"} gap={"14px"}>
+        {[1, 2, 3, 4].map((num, index) => {
+          return (
+            <Box key={index}>
+              <Text
+                width={"158px"}
+                height={"81px"}
+                fontSize={36}
+                fontWeight={700}
+                textAlign={"center"}
+                fontFamily={"oswald"}
+              >
+                Day {num}
+              </Text>
+              <Box display={"flex"}>
+                <Box marginRight={"34px"}>
+                  {events.map((einfo: any) => {
+                    return einfo.dates.map((date: any) => {
+                      const day1 = new Date();
+                      day1.setHours(0, 0, 0, 0);
+                      day1.setDate(new Date().getDate() + num);
+                      const day2 = new Date(date.date);
+
+                      const dayDifference = day1.getDate() - day2.getDate() - 1;
+
+                      const dateObject: DateObject = {
+                        day: dayDifference,
+                        from: date.from,
+                        to: date.to,
+                      };
+
+                      const totalExpense = einfo.expenses.reduce(
+                        (acc: any, cv: any) => acc + cv.cost,
+                        0,
+                      );
+
+                      return (
+                        dayDifference === 0 && (
+                          <CalendarCard
+                            key={einfo.name}
+                            expenseTotal={totalExpense}
+                            date={dateObject}
+                            event={einfo}
+                          />
+                        )
+                      );
+                    });
+                  })}
+                </Box>
+                {num !== 4 && (
+                  <Box width={"1px"} background={"#989898"} height={"788px"} />
+                )}
+              </Box>
+            </Box>
+          );
+        })}
+      </Box>
+    </Flex>
+  );
+}
 
 export default function Calendar() {
-  const [event, setEvent]: any[] = useState([]);
-
   const router = useRouter();
   const { id }: { id?: string } = router.query;
 
-  const chapter: Chapter = trpc.chapter.getChapterByRetreatId.useQuery(
-    id || "123",
-  )?.data!;
-  const { data: currEventData } = trpc.event.getEvents.useQuery(id!, {
+  const chapter: Chapter = trpc.chapter.getChapterByRetreatId.useQuery(id!, {
     enabled: !!id,
-  });
+  })?.data!;
 
-  useEffect(() => {
-    setEvent(currEventData);
-  }, [currEventData]);
-
+  const events = trpc.event.getEvents.useQuery(id!, {
+    enabled: !!id,
+  }).data;
+  return <Container chapter={chapter} year={2023} events={events} />;
   return (
     <Box>
       {<Sidebar chapter={chapter} year={2023} />}
-      {event && (
-        <Flex justifyContent="center" alignItems="center">
-          <Box></Box>
-          <Box display={"flex"} gap={"34px"}>
-            {[1, 2, 3, 4].map((num, index) => {
-              return (
-                <Box key={index}>
-                  <Text
-                    width={"158px"}
-                    height={"81px"}
-                    fontSize={36}
-                    fontWeight={700}
-                    textAlign={"center"}
-                    fontFamily={"oswald"}
-                  >
-                    Day {num}
-                  </Text>
-                  <Box display={"flex"}>
-                    <Box marginRight={"34px"}>
-                      {event.map((einfo: any) => {
-                        return einfo.dates.map((date: any) => {
-                          const day1 = new Date();
-                          day1.setHours(0, 0, 0, 0);
-                          day1.setDate(new Date().getDate() + num);
-                          const day2 = new Date(date.date);
-
-                          const dayDifference =
-                            day1.getDate() - day2.getDate() - 1;
-
-                          const dateObject: DateObject = {
-                            day: dayDifference,
-                            from: date.from,
-                            to: date.to,
-                          };
-
-                          const totalExpense = einfo.expenses.reduce(
-                            (acc: any, cv: any) => acc + cv.cost,
-                            0,
-                          );
-
-                          return (
-                            dayDifference === 0 && (
-                              <CalendarCard
-                                key={einfo.name}
-                                expenseTotal={totalExpense}
-                                date={dateObject}
-                                event={einfo}
-                              />
-                            )
-                          );
-                        });
-                      })}
-                    </Box>
-                    {num !== 4 && (
-                      <Box
-                        width={"1px"}
-                        background={"#989898"}
-                        height={"788px"}
-                      />
-                    )}
-                  </Box>
-                </Box>
-              );
-            })}
-          </Box>
-        </Flex>
-      )}
+      {events && <Content events={events} />}
     </Box>
+  );
+}
+
+function Container({ chapter, year, events }: any) {
+  return (
+    <Grid templateAreas={`'sidebar main'`} gridTemplateColumns={"auto 1fr"}>
+      <GridItem area="sidebar">
+        <Sidebar chapter={chapter} year={year} />
+      </GridItem>
+      <GridItem area="main">{events && <Content events={events} />}</GridItem>
+    </Grid>
   );
 }
