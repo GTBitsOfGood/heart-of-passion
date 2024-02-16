3fd40421506564948222e64a604628a9d1053970
Nabeel Hossain <nhossain34@gatech.edu>
2024-02-16 18:16:24 -0500

    Created donor schema

---
+++ src/server/models/Donor.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+export interface IDonor {
+    _id: string;
+    donorName: string;
+    studentName: string;
+    donorEmail: string;
+    source: "Event 1" | "Event 2" | "Event 3";
+    sponsorLevel: "Platinum" | "Gold" | "Silver" | "Star" | "Bronze"
+}
+
+export const sponsorLevelOptions = ["Platinum", "Gold", "Silver", "Star", "Bronze"];
+
+const DonorSchema = new Schema<IDonor>(
+    {
+        _id: {
+            type: String,
+            required: true,
+            auto: true,
+        },
+        donorName: {
+            type: String,
+        },
+        studentName: {
+            type: String,
+        },
+        donorEmail: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        source: {
+            type: String,
+            enum: ["Event 1", "Event 2", "Event 3"],
+            default: "Event 1",
+            required: true,
+        },
+        sponsorLevel: {
+            type: String,
+            enum: sponsorLevelOptions,
+            required: true,
+        },
+
+    },
+    { _id: false },
+);
+
+export const DonorModel =
+    (mongoose.models.Donor as mongoose.Model<IUser>) ??
+    mongoose.model("User", Donor);
