f28d5bff506be626aadc27eaecdab6b363a6dd68
josephshenq <144394778+josephshenq@users.noreply.github.com>
2024-03-10 13:39:03 -0400

    Testing/sprint2 wiring (#105)
    * add new dummy data
    
    * fix sidebar navigation
    
    * chapter progress bar updating
    
    * sidebar progress update while create/update/delete funds
    
    ---------
    
    Co-authored-by: Jay Nagpaul <jaynagpaul@users.noreply.github.com>

--- src/components/NewFundModal.tsx
+++ src/components/NewFundModal.tsx
@@ -77,16 +77,19 @@ export const NewFundModal = ({
   const updateFund = trpc.fund.updateFund.useMutation({
     onSuccess: () => {
       trpcUtils.fund.invalidate();
+      trpcUtils.chapter.invalidate();
     },
   });
   const createFund = trpc.fund.createFund.useMutation({
     onSuccess: () => {
       trpcUtils.fund.invalidate();
+      trpcUtils.chapter.invalidate();
     },
   });
   const deleteFund = trpc.fund.deleteFund.useMutation({
     onSuccess: () => {
       trpcUtils.fund.invalidate();
+      trpcUtils.chapter.invalidate();
     },
   });
 
