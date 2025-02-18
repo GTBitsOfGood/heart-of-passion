a4bb7d16ea73b4e8e1d87db9767fa4a3d3a42fca
davidlee04 <davidylee31@gmail.com>
2025-02-18 02:46:49 -0500

    Valid year check.

--- src/server/api/routers/donor.ts
+++ src/server/api/routers/donor.ts
@@ -59,6 +59,7 @@ export const donorRouter = createTRPCRouter({
   getDonorsByYear: studentProcedure
     .input(z.number())
     .query(async ({ input }): Promise<Donor[]> => {
+      if(input < 1900) return [];
       const startOfYear = new Date(`${input}-01-01T00:00:00.000Z`);
       const endOfYear = new Date(`${input + 1}-01-01T00:00:00.000Z`);
       const donors = await DonorModel.find({
@@ -79,6 +80,6 @@ function processDonor(obj: any): Donor {
     status: obj.status,
     notes: obj.notes ?? "",
     address: obj.address,
-    createdAt: obj.createdAt,
+    createdAt: obj.createdAt.toISOString(),
   };
 }
